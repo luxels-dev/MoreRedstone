aea395ec6c525b3ae67266a10954c30c9d3dde55
Lux <futurix.z@gmail.com>
2025-07-30 15:54:55 +0200

    command add

--- src/main/java/fr/luxdev/moreRedstone/MoreRedstone.java
+++ src/main/java/fr/luxdev/moreRedstone/MoreRedstone.java
@@ -1,16 +1,19 @@
 package fr.luxdev.moreRedstone;
 
+import fr.luxdev.moreRedstone.Commands.GiveItemCommand;
 import fr.luxdev.moreRedstone.Utils.TagManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MoreRedstone extends JavaPlugin {
 
     private TagManager tagManager;
+    private GiveItemCommand giveItemCommand;
 
     @Override
     public void onEnable() {
 
         tagManager = new TagManager(this);
+        giveItemCommand = new GiveItemCommand(this);
 
     }
 
